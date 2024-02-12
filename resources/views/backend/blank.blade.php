fc66c6df0507bf23f1633dce0dfc649ee9d98a90
bhavdeepbhardwaj <bhavdeepbhardwaj555@gmail.com>
2024-02-12 17:10:13 +0530

    V1.6

--- resources/views/backend/blank.blade.php
+++ resources/views/backend/blank.blade.php
@@ -1,39 +1,31 @@
 @extends('layouts.master')
 
 @section('css')
-    <!-- DataTables -->
-    <link rel="stylesheet" href="{{ asset('backend/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css ') }}">
-    <link rel="stylesheet" href="{{ asset('backend/plugins/datatables-responsive/css/responsive.bootstrap4.min.css ') }}">
-    <link rel="stylesheet" href="{{ asset('backend/plugins/datatables-buttons/css/buttons.bootstrap4.min.css  ') }}">
-
-    <!-- Select2 -->
-    <link rel="stylesheet" href="{{ asset('backend/plugins/select2/css/select2.min.css ') }}">
-    <link rel="stylesheet" href="{{ asset('backend/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css ') }}">
 @endsection
 
 @section('content')
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1>Major-Nom Major Ports and Capacities</h1>
-                    </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Major-Nom Major Ports and Capacities</li>
-                        </ol>
-                    </div>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Major-Nom Major Ports and Capacities</h1>
                 </div>
-            </div><!-- /.container-fluid -->
-        </section>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Major-Nom Major Ports and Capacities</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
 
-        <!-- Main content -->
-        <section class="content">
-            {{-- <form> --}}
+    <!-- Main content -->
+    <section class="content">
+        {{-- <form> --}}
             <div class="container-fluid">
                 <div class="card">
                     <div class="card-header">
@@ -45,132 +37,68 @@
 
                         <form method="POST" action="" enctype="multipart/form-data">
                             @csrf
+                            <!-- START ACCORDION & CAROUSEL-->
                             <div class="row">
-                                <input type="" class="form-control" id="createdBy" value="{{ Auth::user()->id }}"
-                                    name="created_by">
-
+                                <!-- /.col -->
                                 <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>Month <span style="color: red;">*</span></label>
-                                        <select name="select_month" id="select_month"
-                                            class="form-control @error('select_month') is-invalid @enderror">
-                                            <option value="--Select Month--">--Select Month--</option>
-
-                                            {{-- Loop to generate options --}}
-                                            @foreach (range(1, 12) as $monthNumber)
-                                                @php
-                                                    $month = date('F', mktime(0, 0, 0, $monthNumber, 1));
-                                                @endphp
-                                                <option value="{{ $monthNumber }}"
-                                                    {{ old('select_month', date('n')) == $monthNumber ? 'selected' : '' }}>
-                                                    {{ $month }}
-                                                </option>
-                                            @endforeach
-                                        </select>
-
-                                        @error('select_month')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
+                                    <div class="card">
+                                        <!-- /.card-header -->
+                                        <div class="card-body">
+                                            <div id="carouselExampleIndicators" class="carousel slide"
+                                                data-ride="carousel">
+                                                <ol class="carousel-indicators">
+                                                    <li data-target="#carouselExampleIndicators" data-slide-to="0"
+                                                        class="active"></li>
+                                                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                                </ol>
+                                                <div class="carousel-inner">
+                                                    <div class="carousel-item active">
+                                                        <div class="card" style="width: ;">
+                                                            <div class="card-body">
+                                                              <h5 class="card-title">Special title treatment</h5>
+                                                              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                                              <a href="#" class="btn btn-primary">Go somewhere</a>
+                                                            </div>
+                                                          </div>
+                                                    </div>
+                                                    <div class="carousel-item">
+                                                        <img class="d-block w-100"
+                                                            src="https://placehold.it/900x500/3c8dbc/ffffff&text=I+Love+Bootstrap"
+                                                            alt="Second slide">
+                                                    </div>
+                                                    <div class="carousel-item">
+                                                        <img class="d-block w-100"
+                                                            src="https://placehold.it/900x500/f39c12/ffffff&text=I+Love+Bootstrap"
+                                                            alt="Third slide">
+                                                    </div>
+                                                </div>
+                                                <a class="carousel-control-prev" href="#carouselExampleIndicators"
+                                                    role="button" data-slide="prev">
+                                                    <span class="carousel-control-custom-icon" aria-hidden="true">
+                                                        <i class="fas fa-chevron-left"></i>
+                                                    </span>
+                                                    <span class="sr-only">Previous</span>
+                                                </a>
+                                                <a class="carousel-control-next" href="#carouselExampleIndicators"
+                                                    role="button" data-slide="next">
+                                                    <span class="carousel-control-custom-icon" aria-hidden="true">
+                                                        <i class="fas fa-chevron-right"></i>
+                                                    </span>
+                                                    <span class="sr-only">Next</span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <!-- /.card-body -->
                                     </div>
+                                    <!-- /.card -->
                                 </div>
+                                <!-- /.col -->
                             </div>
-
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="port_type">Port Type</label>
-                                        <select class="form-control @error('port_type') is-invalid @enderror"
-                                            name="port_type" id="port_type">
-                                            <option value=''>---Select--</option>
-                                        </select>
-                                        @error('port_type')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </div>
-                                </div>
-
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="state_board">State Board</label>
-                                        <select class="form-control @error('state_board') is-invalid @enderror"
-                                            name="state_board" id="state_board">
-                                            <option value=''>---Select--</option>
-                                        </select>
-                                        @error('state_board')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </div>
-                                </div>
-
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="port_name">Port Name</label>
-                                        <input type="text" class="form-control @error('port_name') is-invalid @enderror"
-                                            id="port_name" placeholder="Enter Name" name="port_name"
-                                            value="{{ old('port_name') }}">
-                                        @error('port_name')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </div>
-                                </div>
-
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="operational">Operational</label>
-                                        <input type="text"
-                                            class="form-control @error('operational') is-invalid @enderror" id="operational"
-                                            placeholder="Enter Operational" name="operational"
-                                            value="{{ old('operational') }}">
-                                        @error('operational')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </div>
-                                </div>
-
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="capacity">Capacity ( IN MILLION METRIC TONNES)</label>
-                                        <input type="text" class="form-control @error('capacity') is-invalid @enderror"
-                                            id="capacity" placeholder="Enter Operational" name="operational"
-                                            value="{{ old('capacity') }}">
-                                        @error('capacity')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </div>
-                                </div>
-
-
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="nonOperational">NON-OPERATIONAL</label>
-                                        <input type="text"
-                                            class="form-control @error('nonOperational') is-invalid @enderror"
-                                            id="nonOperational" placeholder="Enter Operational" name="nonOperational"
-                                            value="{{ old('nonOperational') }}">
-                                        @error('nonOperational')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </div>
-                                </div>
-
-                            </div>
+                            <!-- /.row -->
+                            <!-- END ACCORDION & CAROUSEL-->
                             <div class="d-flex pb-2 justify-content-center align-items-center">
-                                <button type="submit" class="btn btn-primary waves-effect waves-light"
-                                    id="">Add
+                                <button type="submit" class="btn btn-primary waves-effect waves-light" id="">Add
                                     Records</button>
                             </div>
                         </form>
@@ -180,54 +108,14 @@
             {{--
         </form> --}}
 
-        </section>
-        <!--  -->
-    </div>
-    <!-- /.content-wrapper -->
+    </section>
+    <!--  -->
+</div>
+<!-- /.content-wrapper -->
 
-    <!-- MODAL -->
+<!-- MODAL -->
 @endsection
 
 @section('js')
-    <!-- jQuery -->
-    <script src="{{ asset('backend/plugins/jquery/jquery.min.js ') }}"></script>
-    <!-- Bootstrap 4 -->
-    <script src="{{ asset('backend/plugins/bootstrap/js/bootstrap.bundle.min.js ') }}"></script>
-    <!-- DataTables  & Plugins -->
-    <script src="{{ asset('backend/plugins/datatables/jquery.dataTables.min.js ') }}"></script>
-    <script src="{{ asset('backend/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js ') }}"></script>
-    <script src="{{ asset('backend/plugins/datatables-responsive/js/dataTables.responsive.min.js ') }}"></script>
-    <script src="{{ asset('backend/plugins/datatables-responsive/js/responsive.bootstrap4.min.js ') }}"></script>
-    <script src="{{ asset('backend/plugins/datatables-buttons/js/dataTables.buttons.min.js ') }}"></script>
-    <script src="{{ asset('backend/plugins/datatables-buttons/js/buttons.bootstrap4.min.js ') }}"></script>
-    <script src="{{ asset('backend/plugins/jszip/jszip.min.js ') }}"></script>
-    <script src="{{ asset('backend/plugins/pdfmake/pdfmake.min.js ') }}"></script>
-    <script src="{{ asset('backend/plugins/pdfmake/vfs_fonts.js ') }}"></script>
-    <script src="{{ asset('backend/plugins/datatables-buttons/js/buttons.html5.min.js ') }}"></script>
-    <script src="{{ asset('backend/plugins/datatables-buttons/js/buttons.print.min.js ') }}"></script>
-    <script src="{{ asset('backend/plugins/datatables-buttons/js/buttons.colVis.min.js ') }}"></script>
-    <!-- AdminLTE App -->
-    <script src="{{ asset('backend/dist/js/adminlte.min.js ') }}"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="{{ asset('backend/dist/js/demo.js ') }}"></script>
-    <!-- Page specific script -->
-    <script>
-        $(function() {
-            $("#example1").DataTable({
-                "responsive": true,
-                "lengthChange": false,
-                "autoWidth": false,
-                "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-            }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-            $('#example2').DataTable({
-                "paging": true,
-                "lengthChange": false,
-                "searching": false,
-                "ordering": true,
-                "info": true,
-                "autoWidth": false,
-                "responsive": true,
-            });
-        });
-    </script>
+<!-- jQuery -->
 @endsection
